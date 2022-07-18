93864871c34cda95bfcc4e314cefa17ed8fa4129
Mathew <matjenning@gmail.com>
2022-07-18 01:24:57 -0400

    Updates, typos, work descriptions

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,13 +13,13 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta"></span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>Engineering student at Queens University, in my third year of the Mathematics and Engineering, 
+                    <p>I am a engineering student at Queens University, in my final year of the Mathematics and Engineering, 
                         Computing and Communications Program. </p>
-                    <p> I have gainid practical Engineering expirience working as a Embedded Software Engineering Intern 
-                        for 16 months at Cisco Meraki, and as a Intern within the Knowledge Services team at Fasken Martineau DuMoulin LLP.
-                        I am pursuing a Software Engineering career after grauduation, and am looking for positions starting in January 2023.</p>
-                    <p>I have spent my life skiing, and sailing and have spent several years applying my in-depth knowledge to coach children 
-                        in both sports, allowing themto get the best start, and master their skills. </p>
+                    <p>I have gained practical experience working as a Embedded Software Engineering Intern 
+                        for 16 months at Cisco Meraki, and as a Intern within the Knowledge Services team at Fasken Martineau DuMoulin.
+                        I am pursuing a Software Engineering career after grauduation, and am currently applying for positions starting in January 2023.</p>
+                    <p>In my free time I have spent my life skiing, and sailing, spending 6+ years aplling my in-depth knowledge to coach children 
+                        in both sports which allowed them to get the best start and master their skills. </p>
                     </div>
                 </div>
                 </div>
@@ -32,31 +32,31 @@ export default class About extends Component {
             <div className="row">
             <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
                 <span className="heading-meta"></span>
-                <h2 className="colorlib-heading">What I do?</h2>
+                <h2 className="colorlib-heading">My Skillset</h2>
             </div>
             </div>
             <div className="row row-pt-md">
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-1">
                 <span className="icon">
-                    <i className="icon-bulb" />
+                    <i className="icon-phone3" />
                 </span>
                 <div className="desc">
                     <h3>Software Development</h3>
-                    <p>Working at Meraki I developed many algorithims and applications for Linux-kernel based products. 
-                        I also developed embedded software for BLE, LTE, GPS communication and custom drivers for IoT sensors.</p>
+                    <p>Working at Cisco Meraki I developed many algorithims and applications for Linux-kernel based products. 
+                        I developed embedded software for BLE, LTE, GPS communication as well as custom drivers for IoT sensors.</p>
                 </div>
                 </div>
             </div>
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-3">
                 <span className="icon">
-                    <i className="icon-phone3" />
+                    <i className="icon-bulb" />
                 </span>
                 <div className="desc">
                     <h3>Data Structures & Algorithms</h3>
-                    <p>Coming from a Program that specializeddd in both mathematics and computing, I have an in depth knowledge
-                        and understanding of various data structures and algorithims.</p>
+                    <p>Enrolled in a program that specialized in both mathematics and computing, I have an in depth knowledge
+                        and understanding of how various data structures and algorithims are built and operate.</p>
                 </div>
                 </div>
             </div>
@@ -67,8 +67,8 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Data Analytics</h3>
-                    <p>At Fasken, I developed many scripts and web scraping algorithims to keep the Lawyers informed and updated, 
-                        I also seet up Automated systems for Data consumption by leveraging Microsoft Sharepont and Power Suite</p>
+                    <p>At Fasken, I developed python scripts and web scraping algorithims to keep the lawyers informed and up to date. 
+                        I also set up automated systems for data consumption by leveraging Microsoft Sharepont, Power Apps, and Microsoft Azure</p>
                 </div>
                 </div>
             </div>
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -22,7 +22,7 @@ export default class Timeline extends Component {
                       </div>
                       <div className="timeline-label">
                         <h2>Queens's University - Mathematics and Engineering: Computing and Communications <span>Class of 2023</span></h2>
-                        <p></p>
+                        <p>Bachelor of Applied Science - BASc, Mathematics and Engineering Computing and Communications</p>
                       </div>
                     </div>
                   </article>
@@ -33,7 +33,15 @@ export default class Timeline extends Component {
                       </div>
                       <div className="timeline-label">
                         <h2>Summer Intern in Data Analytics - Fasken Martineau DuMoulin LLP <span>Summer 2022</span></h2>
-                        <p></p>
+                        <p>
+                          <ul>
+                            <li>Assisted Fasken’s Knowledge Service Team with data retrieval and analysis.</li>
+                            <li>Created Python functions in Microsoft Azure to scrape information from the web based off lawyers requests.</li>
+                            <li>Set up SQL Databases and Queries for use as a backend for Microsoft Power BI Dashboards</li>
+                            <li>Used Microsoft Power Automate to create flows updating SQL Databases triggered by Automated Azure Functions.</li>
+                            <li>Created python tools for staff to automatically adjust and clean Excel files for lawyer consumption.</li>
+                          </ul>
+                          </p>
                       </div>
                     </div>
                   </article>
@@ -44,7 +52,17 @@ export default class Timeline extends Component {
                       </div>
                       <div className="timeline-label">
                         <h2>Embedded Sotfware Engineering Intern - Cisco Meraki <span>May 2020 - August 2021</span></h2>
-                        <p></p> 
+                        <p>
+                          <ul>
+                            <li>Worked with Meraki’s Platform and Hardware teams on firmware for several Linux-kernel based products (C/C++, Device Tree).</li>
+                            <li>Worked effectively on both individual projects and collaboratively on various 2-10 person teams.</li>
+                            <li>Platform bring up using Yocto and OpenWRT – SoC, PHYs, memory, other peripherals (Make).</li>
+                            <li>Bootloader Development (U-boot).</li>
+                            <li>Embedded software for BLE, LTE, GPS communication (C/C++).</li>
+                            <li>Built Custom drivers for NPI IoT sensors on Meraki’s new MT product line (C).</li>
+                            <li>Worked with vendors such as Qualcomm and Marvell to validate the compatibility of SoCs for upcoming Meraki Products.</li>
+                          </ul>
+                        </p> 
                       </div>
                     </div>
                   </article>
@@ -54,8 +72,13 @@ export default class Timeline extends Component {
                         <i className="icon-book2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Queen's Mostly Autonomous Sailboat Team - Queen's University <span>2017-2019</span></h2>
-                        <p></p>
+                        <h2>Queen's Mostly Autonomous Sailboat Team - Queen's University <span>2017-2020</span></h2>
+                        <p>
+                          <ul>
+                            <li>Head Manager for the Queen’s Autonomous Sailboat Team, leading three separate software development teams to design and program a self-driving boat using image recognition and wind speed / direction detection software.</li>
+                            <li>The boat is entered in annual competitions that test navigation, tactics, and design.</li>
+                          </ul>
+                        </p>
                       </div>
                     </div>
                   </article>
